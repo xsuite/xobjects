e6ad655ea944a91df1f068e1fbd3a88c07891232
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-25 15:04:10 +0100

    Contexts are all tested in test_context.py

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from xobjects import CLContext, ByteArrayContext
+import xobjects as xo
+from xobjects.context import available
 
 
 class Check:
@@ -40,83 +41,97 @@ class Check:
 
 
 def test_cl_print_devices():
-    CLContext.print_devices()
+    xo.ContextPyopencl.print_devices()
 
 
 def test_cl_init():
-    ctx = CLContext(device="0.0")
+    ctx = xo.ContextPyopencl(device="0.0")
 
 
-def test_cl_new_buffer():
-    ctx = CLContext(device="0.0")
-    buff1 = ctx.new_buffer()
-    buff2 = ctx.new_buffer(capacity=100)
 
 
 def test_new_buffer():
-    ctx = ByteArrayContext()
-    buff1 = ctx.new_buffer()
-    buff2 = ctx.new_buffer(capacity=200)
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        buff1 = ctx.new_buffer()
+        buff2 = ctx.new_buffer(capacity=200)
 
 
 def test_read_write():
-    buff = ByteArrayContext().new_buffer()
-    bb = b"asdfasdfafsdf"
-    buff.write(23, bb)
-    assert buff.read(23, len(bb)) == bb
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        buff = ctx.new_buffer()
+        bb = b"asdfasdfafsdf"
+        buff.write(23, bb)
+        assert buff.read(23, len(bb)) == bb
 
-
-def test_cl_read_write():
-    ctx = CLContext(device="0.0")
-    buff = ctx.new_buffer(100)
-    bb = b"asdfasdfafsdf"
-    buff.write(23, bb)
-    assert buff.read(23, len(bb)) == bb
 
 
 def test_allocate_simple():
-    ctx = CLContext(device="0.0")
-    ch = Check(ctx, 200)
-    ch.new_string(30)
-    ch.check()
-
-
-def test_free_simple():
-    ctx = CLContext(device="0.0")
-    ch = Check(ctx, 200)
-    offsets = [ch.new_string(ii * 2 + 1) for ii in range(10)]
-    print(offsets)
-    for offset in offsets:
-        print(offset)
-        ch.free_string(offset)
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        ch = Check(ctx, 200)
+        ch.new_string(30)
         ch.check()
 
 
+def test_free_simple():
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        ch = Check(ctx, 200)
+        offsets = [ch.new_string(ii * 2 + 1) for ii in range(10)]
+        print(offsets)
+        for offset in offsets:
+            print(offset)
+            ch.free_string(offset)
+            ch.check()
+
+
 def test_grow():
-    ctx = CLContext(device="0.0")
-    ch = Check(ctx, 200)
-    ch.new_string(150)
-    ch.new_string(60)
-    ch.check()
-    assert ch.buffer.capacity == 400
-    assert ch.buffer.chunks[0].start == 210
-    assert ch.buffer.chunks[0].end == 400
-    ch.new_string(500)
-    assert ch.buffer.capacity == 900
-    ch.check()
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        ch = Check(ctx, 200)
+        ch.new_string(150)
+        ch.new_string(60)
+        ch.check()
+        assert ch.buffer.capacity == 400
+        assert ch.buffer.chunks[0].start == 210
+        assert ch.buffer.chunks[0].end == 400
+        ch.new_string(500)
+        assert ch.buffer.capacity == 900
+        ch.check()
 
 
 def test_random_string():
 
-    ctx = CLContext(device="0.0")
-    ch = Check(ctx, 200)
+    for CTX in xo.ContextCpu, xo.ContextPyopencl, xo.ContextCupy:
+        if CTX not in available:
+            continue
+        print(f'Test {CTX}')
+        ctx = CTX()
+        ch = Check(ctx, 200)
 
-    for i in range(50):
-        ch.random_string(maxlength=2000)
-        ch.check()
+        for i in range(50):
+            ch.random_string(maxlength=2000)
+            ch.check()
 
-    for i in range(50):
-        ch.random_string(maxlength=2000)
-        ch.check()
-        ch.random_free()
-        ch.check()
+        for i in range(50):
+            ch.random_string(maxlength=2000)
+            ch.check()
+            ch.random_free()
+            ch.check()

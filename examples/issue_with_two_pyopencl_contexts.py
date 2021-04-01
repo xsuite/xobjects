c5839b7dea4a495b17cb069a6576f888014f39e6
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-01 17:17:40 +0200

    Cleaned up

--- examples/issue_with_two_pyopencl_contexts.py
+++ examples/issue_with_two_pyopencl_contexts.py
@@ -6,8 +6,8 @@ a = ctx.zeros((5,5), dtype=np.float64)
 b = a[:2:,:2]
 c = b.copy()
 
-print('Fists done')
-ctx = xo.ContextPyopencl()
-a = ctx.zeros((5,5), dtype=np.float64)
-b = a[:2:,:2]
-c = b.copy()
+print('First done')
+ctx2 = xo.ContextPyopencl()
+aa = ctx2.zeros((5,5), dtype=np.float64)
+bb = aa[:2:,:2]
+cc = bb.copy()

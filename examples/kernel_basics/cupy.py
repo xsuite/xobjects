3e04a0ac259ecde2fef99e81663a988109cbb3ed
Riccardo De Maria <riccardodemaria@gmail.com>
2021-04-15 10:37:15 +0200

    renaming

--- examples/kernel_basics/cupy.py
+++ examples/kernel_basics/cupy.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 import cupy
 
-source_str = r'''
+source_str = r"""
 extern "C"{
 
 __global__
@@ -16,24 +15,23 @@ void mymul(int n,
     }
 }
 
-}'''
+}"""
 module = cupy.RawModule(code=source_str)
-mymul_kernel = module.get_function('mymul')
+mymul_kernel = module.get_function("mymul")
 
-x1 = np.array([1,2,3,4,5], dtype=np.float64)
-x2 = np.array([7,8,9,10,12], dtype=np.float64)
+x1 = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+x2 = np.array([7, 8, 9, 10, 12], dtype=np.float64)
 
 x1_dev = cupy.array(x1)
 x2_dev = cupy.array(x2)
 y_dev = cupy.zeros_like(x1_dev)
 
-blocksize=2
-n_blocks = int(np.ceil(len(x1)/blocksize))
-mymul_kernel(grid=(n_blocks,), block=(blocksize,),
-        args=(len(x1), x1_dev, x2_dev, y_dev))
+blocksize = 2
+n_blocks = int(np.ceil(len(x1) / blocksize))
+mymul_kernel(
+    grid=(n_blocks,), block=(blocksize,), args=(len(x1), x1_dev, x2_dev, y_dev)
+)
 
 y = y_dev.get()
 
-assert np.allclose(y, x1*x2)
-
-
+assert np.allclose(y, x1 * x2)

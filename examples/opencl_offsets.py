722e788b463038dc0e18ad0632d40a0f7be048e9
Riccardo De Maria <riccardodemaria@gmail.com>
2021-04-15 20:38:56 +0200

    upadte

--- examples/opencl_offsets.py
+++ examples/opencl_offsets.py
@@ -5,47 +5,28 @@ import pyopencl.array as cl_array
 def try_offset(ctx):
     queue = cl.CommandQueue(ctx)
 
-    a = cl_array.Array(queue, shape=(10,), dtype="uint8")
+    a = cl_array.Array(queue, shape=(2048,), dtype="uint8")
     b = a[3:]
 
     try:
         b.data
     except Exception as ex:
-        print("Always fails due to ArrayHasOffsetError")
-        print(repr(ex))
+        pass
+        # print("Always fails due to ArrayHasOffsetError")
+        #print(repr(ex))
 
-    try:
-        b = a[3:]
-        b.base_data[b.offset : b.offset + b.nbytes]
-        print(f"offset={b.offset} OK")
-    except Exception as ex:
-        print(f"offset={b.offset} not OK")
-        print(
-            "Fails in some platform due to clCreateSubBuffer MISALIGNED_SUB_BUFFER_OFFSET"
-        )
-        print(repr(ex))
-
-    try:
-        b = a[4:]
-        b.base_data[b.offset : b.offset + b.nbytes]
-        print(f"offset={b.offset} OK")
-    except Exception as ex:
-        print(f"offset={b.offset} not OK")
-        print("Does not fail easily, but still can fail")
-        print(repr(ex))
-
-    try:
-        b = a[8:]
-        b.base_data[b.offset : b.offset + b.nbytes]
-        print(f"offset={b.offset} OK")
-    except Exception as ex:
-        print(f"offset={b.offset} not OK")
-        print("Does not fail easily, but still can fail")
-        print(repr(ex))
-
+    for offset in 3,4,8,16,32,64,128:
+        try:
+            b = a[offset:]
+            b.base_data[b.offset : b.offset + b.nbytes]
+            print(f"offset={b.offset} OK")
+        except Exception as ex:
+            # Fails in some platform due to clCreateSubBuffer MISALIGNED_SUB_BUFFER_OFFSET"
+            print(f"offset={b.offset} not OK")
+            #print(repr(ex))
 
 for platform in cl.get_platforms():
     for device in platform.get_devices():
         ctx = cl.Context(devices=[device])
-        print(ctx)
+        print(f"{ctx.devices[0].name} on {ctx.devices[0].platform.name}")
         try_offset(ctx)

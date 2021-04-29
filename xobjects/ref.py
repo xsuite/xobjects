4061a38ab412f29bb258ace16d17d7d965c1601d
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-29 09:25:16 +0200

    Unionref implemented but untested, ref functionality works

--- xobjects/ref.py
+++ xobjects/ref.py
@@ -9,31 +9,77 @@ class MetaRef(type):
         return cls(rtype)
 
 class Ref(metaclass=MetaRef):
-    _size = 8
 
     def __init__(self, rtype):
-        self._rtype = rtype
+        if hasattr(rtype,' __iter__'):
+            self._rtypes = rtype
+            self._isunion = True
+            self._size = 16
+        else:
+            self._rtype = rtype
+            self._isunion = False
+            self._size = 8
+
+    def _typeid_from_type(self, typ):
+        for ii, tt in self._rtypes:
+            if typ is tt:
+                return ii
+        # If no match found:
+        raise TypeError(f'{typ} not registered types!')
+
+    def _type_from_typeid(self, typeid):
+        for ii, tt in self._rtypes:
+            if ii == typeid:
+                return tt
+        # If no match found:
+        raise TypeError(f'Invalid id: {typeid}!')
+
+    def _get_stored_type(self):
+        typeid = Int64._from_buffer(buffer, offset + 8)
+        return self._type_from_typeid(typeid)
 
     def _from_buffer(self, buffer, offset):
         refoffset = Int64._from_buffer(buffer, offset)
-        return self._rtype._from_buffer(buffer, refoffset)
+        if self._isunion:
+            rtype = self._get_stored_type()
+        else:
+            rtype = self._rtype
+        return rtype._from_buffer(buffer, refoffset)
 
     def _to_buffer(self, buffer, offset, value, info=None):
 
-        if (isinstance(value, self._rtype)
-                and value._buffer is buffer):
-            refoffset = value._offset
-        else:
-            if np.isscalar(value):
-                refoffset = int(value)
+        # Get/set reference type
+        if self._isunion:
+            if (hasattr(value, '__class__')
+                and value.__class__ in self._rtypes):
+                rtype = value.__class__
+                typeid = self._typeid_from_type(rtype)
+                Int64._to_buffer(buffer, offset + 8, typeid)
+            elif value is None:
+                # Use the first type (default)
+                rtype = self._rtypes[0]
+                Int64._to_buffer(buffer, offset + 8, 0)
             else:
-                newobj = self._rtype(value, _buffer=buffer)
+                # Keep old type
+                rtype = self._get_stored_type()
+        else:
+            rtype = self._rtype
+
+        # Get/set content
+        if (isinstance(value, rtype)
+                    and value._buffer is buffer):
+                refoffset = value._offset
+        else:
+            if value is None:
+                refoffset = -1
+            else:
+                newobj = rtype(value, _buffer=buffer)
                 refoffset = newobj._offset
         Int64._to_buffer(buffer, offset, refoffset)
 
     def __call__(self, value=None):
         if value is None:
-            return -1
+            return None
         else:
             raise NotImplementedError
 

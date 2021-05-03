b8739fe2e607b2b5bc566b72efa7eb2db1026a07
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-03 19:50:46 +0200

    Introduced additional test showing capi issue

--- tests/test_capi.py
+++ tests/test_capi.py
@@ -170,3 +170,21 @@ def test_capi_call():
         )
 
     assert context.kernels.ParticlesData_get_x(obj=particles, i0=1) == particles.x[1]
+
+def test_2_particles():
+    context = xo.ContextCpu()
+
+    class ParticlesData(xo.Struct):
+        num_particles = xo.Int64
+        s = xo.Float64[:]
+        x = xo.Float64[:]
+
+    particles = ParticlesData(
+                    num_particles=2,
+                    s=np.array([1,2,]),
+                    x=np.array([7,8,]))
+
+    source, kernels, cdefs = ParticlesData._gen_c_api()
+    context.add_kernels([source], kernels, extra_cdef=cdefs)
+
+    assert context.kernels.ParticlesData_get_x(obj=particles, i0=0) == particles.x[0]

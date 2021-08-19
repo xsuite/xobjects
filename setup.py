6ba1f11a001727727dc101c97bd8e27cb663016a
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-08-19 18:59:13 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="xobjects",
-    version="0.0.7",
+    version="0.0.8",
     description="In-memory serialization and code generator for CPU and GPU",
     author="Riccardo De Maria",
     author_email="riccardo.de.maria@cern.ch",

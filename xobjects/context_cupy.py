0ee05c5c3ef94bd802c79ca2da1942b2a76ec99f
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-09-30 16:55:53 +0200

    cosmetic changes

--- xobjects/context_cupy.py
+++ xobjects/context_cupy.py
@@ -33,7 +33,7 @@ except ImportError:
 
 
 cudaheader = [
-"""\
+    """\
 typedef signed long long int64_t;  //only_for_context cuda
 typedef signed char      int8_t;   //only_for_context cuda
 typedef unsigned int     uint32_t; //only_for_context cuda

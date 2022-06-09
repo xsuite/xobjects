904f992333be905664290e93d8470ece71e86b1e
giadarol <giovanni.iadarola@cern.ch>
2022-06-09 18:35:26 +0200

    Add metadata in setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,21 @@ from setuptools import setup
 
 setup(
     name="xobjects",
-    version="0.1.15",
+    version="0.1.16",
     description="In-memory serialization and code generator for CPU and GPU",
+    long_description="In-memory serialization and code generator for CPU and GPU",
     author="Riccardo De Maria",
     author_email="riccardo.de.maria@cern.ch",
-    url="https://github.com/rdemaria/cobjects",
+    url='https://xsuite.readthedocs.io/',
     python_requires=">=3.7",
     setup_requires=[],
     install_requires=["numpy", "cffi"],
     packages=["xobjects"],
+    license='Apache 2.0',
+    download_url="https://pypi.python.org/pypi/xobjects",
+    project_urls={
+            "Bug Tracker": "https://github.com/xsuite/xsuite/issues",
+            "Documentation": 'https://xsuite.readthedocs.io/',
+            "Source Code": "https://github.com/xsuite/xobjects",
+        },
 )

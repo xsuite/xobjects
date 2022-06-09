55650b472c39d0178028f551b37c11eb6101cb45
giadarol <giovanni.iadarola@cern.ch>
2022-06-09 08:50:11 +0200

    Added license and contributors

---
+++ update_copyright_statement.py
@@ -0,0 +1,48 @@
+# copyright ############################### #
+# This file is part of the Xpart Package.   #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+import os
+
+copyright_statement = (
+"""copyright ###############################
+This file is part of the Xpart Package.  
+Copyright (c) CERN, 2021.                
+#########################################""")
+
+config = [
+    {'extension': '.py', 'comment_char': '#'},
+    {'extension': '.h', 'comment_char': '//'},
+]
+
+for cc in config:
+    extension =  cc['extension']
+    comment_char = cc['comment_char']
+
+    cpright_lines = [comment_char + ' ' + line + " " + comment_char + '\n'
+                        for line in copyright_statement.splitlines()
+                        ] + ['\n']
+
+    for root, dirs, files in os.walk("./"):
+        for fname in files:
+            if fname.endswith(extension):
+                file = os.path.join(root, fname)
+                print(file)
+                with open(file, 'r') as fid:
+                    lines = fid.readlines()
+                # Remove copyright statement if present
+                if (len(lines) > 1 and
+                    lines[0].startswith(comment_char + ' ' + 'copyright ##')):
+                    for ill, ll in enumerate(lines):
+                        assert ll.startswith(comment_char)
+                        if ll.startswith(comment_char + ' ' + '########'):
+                            end_cpright = ill
+                            break
+                    assert lines[end_cpright+1] == '\n'
+                    lines = lines[end_cpright+2:]
+
+                lines = cpright_lines + lines
+
+                with open(file, 'w') as fid:
+                    fid.writelines(lines)

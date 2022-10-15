2a80821f5716da1289d3cd823bc4ad2fae90f1a3
giadarol <giovanni.iadarola@cern.ch>
2022-10-15 23:03:11 +0200

    Fix in tests

--- tests/test_typeutils.py
+++ tests/test_typeutils.py
@@ -4,52 +4,55 @@
 # ########################################### #
 
 import xobjects as xo
+from xobjects.typeutils import allocate_on_buffer
 
 import pytest
 
 
-def test_get_a_buffer_ctx_none_buff_none_offset_none():
-    buffer, offset = xo.get_a_buffer(size=16)
+
+def test_allocate_on_buffer_ctx_none_buff_none_offset_none():
+    buffer, offset = allocate_on_buffer(size=16)
     assert buffer.context.__class__ == xo.context_default.__class__
     assert offset == 0
     assert buffer.capacity == 16
 
 
-def test_get_a_buffer_ctx_none_buff_none_offset_given():
+def test_allocate_on_buffer_ctx_none_buff_none_offset_given():
     with pytest.raises(ValueError):
-        _ = xo.get_a_buffer(size=16, offset=5)
+        _ = allocate_on_buffer(size=16, offset=5)
 
 
-def test_get_a_buffer_ctx_none_buff_given_offset_none():
+def test_allocate_on_buffer_ctx_none_buff_given_offset_none():
     original_buffer = xo.context_default.new_buffer(16)
-    buffer, offset = xo.get_a_buffer(size=16, buffer=original_buffer)
+    buffer, offset = allocate_on_buffer(size=16, buffer=original_buffer)
     assert buffer is original_buffer
     assert buffer.capacity >= 16
 
 
-def test_get_a_buffer_ctx_none_buff_given_offset_given():
+def test_allocate_on_buffer_ctx_none_buff_given_offset_given():
     original_buffer = xo.context_default.new_buffer(16)
-    with pytest.raises(ValueError):
-        _ = xo.get_a_buffer(size=16, buffer=original_buffer, offset=3)
+    buffer, offset = allocate_on_buffer(size=16, buffer=original_buffer, offset=3)
+    assert buffer is original_buffer
+    assert offset == 3
 
 
-def test_get_a_buffer_ctx_given_buff_none_offset_none():
+def test_allocate_on_buffer_ctx_given_buff_none_offset_none():
     context = xo.ContextCpu()
-    buffer, offset = xo.get_a_buffer(size=32, context=context)
+    buffer, offset = allocate_on_buffer(size=32, context=context)
     assert buffer.context is context
     assert offset == 0
     assert buffer.capacity >= 32
 
 
-def test_get_a_buffer_ctx_given_buff_none_offset_given():
+def test_allocate_on_buffer_ctx_given_buff_none_offset_given():
     with pytest.raises(ValueError):
-        _ = xo.get_a_buffer(size=16, offset=5)
+        _ = allocate_on_buffer(size=16, offset=5)
 
 
-def test_get_a_buffer_ctx_given_buff_given_offset_none_ok():
+def test_allocate_on_buffer_ctx_given_buff_given_offset_none_ok():
     context = xo.ContextCpu()
     original_buffer = context.new_buffer(16)
-    buffer, offset = xo.get_a_buffer(
+    buffer, offset = allocate_on_buffer(
         size=32, context=context, buffer=original_buffer
     )
     assert buffer is original_buffer
@@ -58,14 +61,14 @@ def test_get_a_buffer_ctx_given_buff_given_offset_none_ok():
     assert buffer.capacity >= 32
 
 
-def test_get_a_buffer_ctx_given_buff_given_offset_none_fail():
+def test_allocate_on_buffer_ctx_given_buff_given_offset_none():
     context1, context2 = xo.ContextCpu(), xo.ContextCpu()
     assert context1 is not context2
 
     buffer_on_context1 = context1.new_buffer(16)
 
     with pytest.raises(ValueError):
-        _ = xo.get_a_buffer(
+        _ = allocate_on_buffer(
             size=16,
             context=context2,
             buffer=buffer_on_context1,
@@ -73,10 +76,10 @@ def test_get_a_buffer_ctx_given_buff_given_offset_none_fail():
 
 
 @pytest.mark.parametrize('input_offset', ['packed', 'aligned'])
-def test_get_a_buffer_ctx_given_buff_given_offset_given_ok(input_offset):
+def test_allocate_on_buffer_ctx_given_buff_given_offset_given_ok(input_offset):
     context = xo.ContextCpu()
     original_buffer = context.new_buffer(16)
-    buffer, offset = xo.get_a_buffer(
+    buffer, offset = allocate_on_buffer(
         size=32,
         context=context,
         buffer=original_buffer,
@@ -87,13 +90,14 @@ def test_get_a_buffer_ctx_given_buff_given_offset_given_ok(input_offset):
     assert buffer.capacity >= 32
 
 
-def test_get_a_buffer_ctx_given_buff_given_offset_given_fail():
+def test_allocate_on_buffer_ctx_given_buff_given_offset_given():
     context = xo.ContextCpu()
-    original_buffer = context.new_buffer(16)
-    with pytest.raises(ValueError):
-        buffer, offset = xo.get_a_buffer(
+    original_buffer = context.new_buffer(10000)
+    buffer, offset = allocate_on_buffer(
             size=32,
             context=context,
             buffer=original_buffer,
             offset=1234,
         )
+    assert buffer is original_buffer
+    assert offset == 1234

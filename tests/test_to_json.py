4dc211e800d072295a7b0481831f1a4756956826
Riccardo De Maria <riccardodemaria@gmail.com>
2022-11-22 17:06:51 +0100

    black

--- tests/test_to_json.py
+++ tests/test_to_json.py
@@ -2,25 +2,22 @@ def test_to_json():
     import xobjects as xo
 
     class A(xo.Struct):
-        a=xo.Float64[:]
-
+        a = xo.Float64[:]
 
     class B(xo.Struct):
-        c=xo.Float64[:]
-        d=xo.Int64
+        c = xo.Float64[:]
+        d = xo.Int64
 
     class Uref(xo.UnionRef):
-        _reftypes=A,B
+        _reftypes = A, B
 
-    AUref=Uref[:]
+    AUref = Uref[:]
 
-    a=AUref(10)
-    a[1]=A(a=[3])
-    a[5]=B(c=2,d=1)
+    a = AUref(10)
+    a[1] = A(a=[3])
+    a[5] = B(c=2, d=1)
 
-    b=AUref(a._to_json())
+    b = AUref(a._to_json())
 
-    assert b[1].a[0]==3
-    assert b[5].d==1
-
-
+    assert b[1].a[0] == 3
+    assert b[5].d == 1

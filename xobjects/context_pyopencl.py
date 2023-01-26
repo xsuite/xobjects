a3d70cbb2d0c1dcce5b8d66acc1444f68550f047
Philipp Niedermayer <eltos@outlook.de>
2023-01-26 16:37:51 +0000

    Fix missing type definitions
    Fixes https://github.com/xsuite/xsuite/issues/283

--- xobjects/context_pyopencl.py
+++ xobjects/context_pyopencl.py
@@ -47,9 +47,14 @@ from ._patch_pyopencl_array import _patch_pyopencl_array
 openclheader: List[SourceType] = [
     """\
 #ifndef XOBJ_STDINT
-typedef long int64_t;
-typedef char int8_t;
-typedef unsigned int uint32_t;
+typedef long           int64_t;
+typedef int            int32_t;
+typedef short          int16_t;
+typedef char           int8_t;
+typedef unsigned long  uint64_t;
+typedef unsigned int   uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char  uint8_t;
 #endif
 #ifndef NULL
 #define NULL 0L

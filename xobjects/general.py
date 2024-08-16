ebb22a4a33573749e5a7f95dad61bf1d50e1fad5
Riccardo De Maria <riccardodemaria@gmail.com>
2024-08-16 14:57:38 +0200

    merge

--- xobjects/general.py
+++ xobjects/general.py
@@ -3,6 +3,7 @@
 # Copyright (c) CERN, 2024.                   #
 # ########################################### #
 from numpy.testing import assert_allclose as np_assert_allclose
+import numpy as np
 
 
 class Print:
@@ -21,4 +22,12 @@ def assert_allclose(a, b, rtol=1e-7, atol=1e-7):
         a = a.get()
     if hasattr(b, "get"):
         b = b.get()
+    try:
+        a = np.squeeze(a)
+    except:
+        pass
+    try:
+        b = np.squeeze(b)
+    except:
+        pass
     np_assert_allclose(a, b, rtol=rtol, atol=atol)

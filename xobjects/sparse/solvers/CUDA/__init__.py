79bc3499d44f173d87394a36aec55f335b187bf8
Evangelos Katralis <vkatralis@gmail.com>
2025-12-02 11:58:42 +0100

    Update solver interface so that it is consistent accross platforms

--- xobjects/sparse/solvers/CUDA/__init__.py
+++ xobjects/sparse/solvers/CUDA/__init__.py
@@ -1,16 +1,24 @@
-__all__ = []
+from ....context import ModuleNotAvailableError
 try:
     from ._cuDSSLU import DirectSolverSuperLU as cuDSSSuperLU
-    __all__.append("cuDSSSuperLU")
-except (ModuleNotFoundError,ImportError):
-    pass
+except (ModuleNotFoundError,ImportError) as e:
+    def cuDSSSuperLU(*args, **kwargs):
+        raise ModuleNotAvailableError(
+            "cuDSSSuperLU is not available. Could not import required backend."
+            ) from e
 try:
     from ._luLU import luLU as CachedSuperLU
-    __all__.append("CachedSuperLU")
-    from cupyx.scipy.sparse.linalg import splu as CupySuperLU
-    __all__.append("CupySuperLU")
 except (ModuleNotFoundError,ImportError):
-    pass
+    def CachedSuperLU(*args, **kwargs):
+        raise ModuleNotAvailableError(
+            "CachedSuperLU is not available. Could not import required backend."
+            ) from e
+try:
+    from cupyx.scipy.sparse.linalg import splu as CupySuperLU
+except (ModuleNotFoundError,ImportError):
+    def CupySuperLU(*args, **kwargs):
+        raise ModuleNotAvailableError(
+            "CupySuperLU is not available. Could not import required backend."
+            ) from e
 
-if not __all__:
-    raise ImportError("Failed to import any CUDA-based sparse solver")
+__all__ = ["cuDSSSuperLU", "CachedSuperLU", "CupySuperLU"]
--- xobjects/sparse/solvers/__init__.py
+++ xobjects/sparse/solvers/__init__.py
@@ -1,7 +1,3 @@
 from . import CPU
-__all__ = ["CPU"]
-try:
-    from . import CUDA
-    __all__.append("CUDA")
-except ImportError:
-    pass
+from . import CUDA
+__all__ = ["CPU", "CUDA"]

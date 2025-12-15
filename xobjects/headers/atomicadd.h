19258bdf406e47b157c2d0c5a8f4d7e718168e08
Evangelos Katralis <vkatralis@gmail.com>
2025-12-15 14:47:58 +0100

    Clean up and add comments

--- xobjects/headers/atomicadd.h
+++ xobjects/headers/atomicadd.h
@@ -112,14 +112,13 @@ DEF_ATOMIC_ADD(double  , f64)
         typedef unsigned int       uint32_t;
         typedef unsigned long long uint64_t;
     #elif defined(__HIPCC__) && !defined(__CUDACC_RTC__)
+        // ROCm appears to have definitions for 64-bit int types
         typedef signed char        int8_t;
         typedef short              int16_t;
         typedef int                int32_t;
-        // typedef long long          int64_t;
         typedef unsigned char      uint8_t;
         typedef unsigned short     uint16_t;
         typedef unsigned int       uint32_t;
-        // typedef unsigned long long uint64_t;
     #else
         // Alternatively, NVCC path is fine with host headers
         #include <stdint.h>
